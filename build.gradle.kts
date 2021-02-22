dce69452110aeb592b0bbd0c280f6da63e0b9a90
jobinlawrance <jobinlawrance@gmail.com>
2021-02-22 20:02:38 +0530

    Add tesseract to detect text in print from images

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.3"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.30"
-	kotlin("plugin.spring") version "1.4.30"
+    id("org.springframework.boot") version "2.4.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.4.30"
+    kotlin("plugin.spring") version "1.4.30"
 }
 
 group = "com.jobinlawrance"
@@ -12,26 +12,41 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
+val kotlin_version = "1.4.30"
+val kotlin_coroutine_version = "1.4.2"
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlin_coroutine_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$kotlin_coroutine_version")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("net.sourceforge.tess4j:tess4j:4.1.1")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("io.ktor:ktor-client-core:1.5.1")
+    implementation("io.ktor:ktor-client-cio:1.5.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/com/jobinlawrance/imageocr/ImageOcrApplication.kt
+++ src/main/kotlin/com/jobinlawrance/imageocr/ImageOcrApplication.kt
@@ -1,22 +1,69 @@
 package com.jobinlawrance.imageocr
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.util.cio.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.*
+import net.sourceforge.tess4j.Tesseract
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
+import java.net.URL
+import java.nio.file.Paths
 
 @RestController
 @SpringBootApplication
 class ImageOcrApplication {
 
-	@GetMapping("/hello" )
-	fun home(): String? {
-		return "Hello World!"
-	}
+    @GetMapping("/hello")
+    fun home(): String? {
+        return "Hello World!"
+    }
 
+    @GetMapping("/ocr/printedText")
+    suspend fun ocr(@RequestParam imageUrl: String): String {
+        val client = HttpClient(CIO)
+        val file = File("${Paths.get("").toAbsolutePath()}/${imageUrl.substring(imageUrl.lastIndexOf("/") + 1)}")
+        val tesserract = Tesseract()
+        return withContext(Dispatchers.Default) {
+            client.downloadFile(file, imageUrl)
+            tesserract.setDatapath("C:/Users/jobin/Downloads/tesdata")
+            try {
+                tesserract.doOCR(file) ?: ""
+            } catch (e: Exception) {
+                e.localizedMessage
+            } finally {
+                delay(1000)
+                file.delete()
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<ImageOcrApplication>(*args)
+    runApplication<ImageOcrApplication>(*args)
 }
+
+suspend fun HttpClient.downloadFile(file: File, url: String): Boolean {
+
+    val response = request<HttpStatement> {
+        url(url)
+        method = HttpMethod.Get
+    }.execute()
+
+    return if (response.status.isSuccess()) {
+        withContext(Dispatchers.IO) {
+            response.content.copyAndClose(file.writeChannel())
+        }
+        true
+    } else false
+
+}
+
